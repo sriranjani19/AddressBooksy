81a9d986114df27cf1ae4b6b30110623c8fcc88a
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-03-16 15:24:49 +0530

    program

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿@@ -0,0 + 1,23 @@
+using System;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book \n");
+
+            AddressBookMain addressBookMain = new AddressBookMain();
+            addressBookMain.AddressBook();
+
+            //AddUsingConsole addUsingConsole = new AddUsingConsole();
+            //addUsingConsole.readUserDetails();
+
+            EditPersonDetails editPersonDetails = new EditPersonDetails();
+            editPersonDetails.editUserDetails();
+
+        }
+    }
+
+}
+0 comments on commit 2c6ab36
+
